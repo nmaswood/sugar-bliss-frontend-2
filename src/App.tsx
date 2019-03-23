26d3aed36d6a17c2e463679e1037a29959fbe303
nmaswood <nmaswood@civisanalytics.com>
2019-03-23 16:04:52 -0700

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Main } from "./Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Main />
-
-        <div className="field">
-          <div className="control">
-            <input
-              className="input is-info"
-              type="text"
-              placeholder="Info input"
-            />
-          </div>
-        </div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="main-header">Sugar Bliss Calculator</div>
       </div>
     );
   }
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,59 @@
 import * as React from "react";
+import Autocomplete from "react-autocomplete";
 
-interface State {}
-interface Props {}
+interface State {
+  value: string;
+}
+interface Props {
+  timeOptions?: string[];
+  dropdownOptions?: string[];
+  orderOptions?: string[];
+}
 
-export class Main extends React.PureComponent<Props, State> {
+export class InfoForm extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { value: "" };
+  }
+
   render() {
-    return <div>Sugar Bliss Calculator</div>;
+    return (
+      <div className="field is-grouped">
+        <div className="control">
+          <label className="label">Zipcode</label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              placeholder="Text input"
+              pattern="[0-9]*"
+            />
+          </div>
+        </div>
+
+        <div className="control">
+          <label className="label">Time</label>
+          <div className="control">
+            <input className="input" type="date" />
+          </div>
+        </div>
+
+        <div className="control">
+          <label className="label">Time Range</label>
+          <div className="select">
+            <select>
+              <option>Select dropdown</option>
+              <option>With options</option>
+            </select>
+          </div>
+        </div>
+        <Autocomplete
+          items={[]}
+          getItemValue={item => item}
+          renderItem={item => <div key={item}>{item}</div>}
+          value={"foo"}
+        />
+      </div>
+    );
   }
 }
