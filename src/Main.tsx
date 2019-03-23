0a8e4c5a8295ae31c7c8dac8ac11dcfc1cf171ac
nmaswood <nmaswood@civisanalytics.com>
2019-03-23 14:34:31 -0700

    commit files

---
+++ src/Main.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+//interface State {
+
+//}
+
+//interface Props {
+//path: string;
+//hasAccess: boolean;
+//render: () => React.ReactNode;
+//}
+
+//export class AccessCheckingRoute extends React.PureComponent<Props, State> {
+//render() {
+//const { path, hasAccess, render } = this.props;
+//return <Route path={path} component={hasAccess ? undefined : NoAccess} render={hasAccess ? render : undefined} />;
+//}
+//}
